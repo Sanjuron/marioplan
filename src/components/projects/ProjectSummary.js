b4a2210e9351c5a01d0c8387b3d7b43dbd7579aa
Sanjuron <julien_nn@live.fr>
2020-07-07 17:03:22 +0200

    #6 -composant liste

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+const ProjectSummary = () => {
+    return (
+    <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Titre du projet</span>
+            <p>Posté par Sanjuron</p>
+            <p className="grey-text">7 juillet 2020</p>
+        </div>
+    </div>
+
+    )
+}
+
+export default ProjectSummary;
